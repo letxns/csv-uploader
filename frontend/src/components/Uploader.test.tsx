6b4312fd390a85691f92f1898c68b03f4f4d6f0f
Leticia Martins <smartins.leticia@hotmail.com>
2023-09-29 22:29:15 -0300

    feat: add uploader component, style and test with jest

---
+++ frontend/src/components/Uploader.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Uploader } from './Uploader.tsx'; 
+import '@testing-library/jest-dom'
+
+test('renders upload button', () => {
+  render(<Uploader />);
+  const uploadButton = screen.getByText('Upload file');
+  expect(uploadButton).toBeInTheDocument();
+});
+
+test('displays an error message for invalid file type', () => {
+    render(<Uploader />);
+    const fileInput = screen.getByLabelText('Upload file');
+  
+    // Simule o carregamento de um arquivo inválido
+    const file = new File(['file content'], 'file.txt', { type: 'text/plain' });
+    fireEvent.change(fileInput, { target: { files: [file] } });
+  
+    const errorMessage = screen.getByText('Please upload a .csv file');
+    expect(errorMessage).toBeInTheDocument();
+  });
+  
---
+++ frontend/src/components/Uploader.tsx
@@ -0,0 +1,95 @@
+import { useRef, useState, ChangeEvent } from 'react'
+import { IFileData, ICSVRow } from '../models/Uploader'
+import './uploader.css'
+
+export const Uploader = () => {
+  const [uploadError, setUploadError] = useState('')
+  const [fileData, setFileData] = useState<IFileData | undefined>(undefined);
+  const [csvRows, setCSVRows] = useState<ICSVRow[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>(''); 
+  const uploadRef = useRef<HTMLInputElement>(null)
+
+  const handleUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) {
+      return
+    }
+    const file = e.target.files[0]
+
+    if (file) {
+      if (file.type !== 'text/csv') {
+        setUploadError('Please upload a .csv file');
+        return;
+      }
+
+      const fileReader = new FileReader()
+      fileReader.onload = (event) => {
+        const content:string = event?.target?.result as string;
+        setFileData({ content })
+        console.log(content)
+        if (content) {
+            const rows = (content as string).split('\n');
+            if (rows.length > 0) {
+              const dataRows = rows.slice(1).map((row: string) => {
+                const values = row.split(',');
+                const rowData: ICSVRow = {
+                  name: values[0], 
+                  city: values[1],
+                  country: values[2],
+                  favorite_sport: values[2],
+                };
+                return rowData;
+              });
+              setCSVRows(dataRows);
+            }
+          }
+      }
+
+      e.target.value = ''
+      fileReader.readAsText(file)
+    } else {
+      setUploadError('File could not be uploaded. Please try again.')
+    }
+  }
+
+  const filteredCSVRows = csvRows.filter((row) => {
+    return row.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.country.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.favorite_sport.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <section>
+        <button onClick={() => uploadRef.current?.click()} className='btn-upload'>Click to upload CSV file</button>
+        <input
+            type="file"
+            accept=".csv"
+            ref={uploadRef}
+            onChange={handleUpload}
+            style={{ display: 'none' }}
+        />
+
+        <article>
+            <input 
+                type="text" 
+                placeholder="Search for data"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </article>
+
+        <div className="card-container">
+            {filteredCSVRows.map((row, index) => (
+            <div className="card" key={index}>
+                <h3>name: {row.name}</h3>
+                <p>city: {row.city}</p>
+                <p>country: {row.country}</p>
+                <p>favorite_sport: {row.favorite_sport}</p>
+            </div>
+            ))}
+        </div>
+
+        {uploadError ? <p>{uploadError}</p> : null}
+    </section>
+  )
+}
