f761656b8e949c33324eb23fbaf0d8ab3c225113
Leticia Martins <smartins.leticia@hotmail.com>
2023-09-29 22:27:28 -0300

    feat: add first application route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home';
+import { Home } from './containers/Home';
 import './App.css'
 
 function App() {
